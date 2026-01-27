d669c2bcccf909acdc88ad55734e6f866b815e2c
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-27 21:18:58 +0200

    feat: using router to routing between pages based on the url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/login-page/login-page').then((m) => m.LoginPage),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
