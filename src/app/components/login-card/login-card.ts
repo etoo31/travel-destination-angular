9fc5da9529d4b50a94b1173b7592f4da06dbb522
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-27 20:13:45 +0200

    feat: created login form component and use it in login card component

--- src/app/components/login-card/login-card.ts
+++ src/app/components/login-card/login-card.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginForm } from '../login-form/login-form';
 
 @Component({
   selector: 'app-login-card',
-  imports: [],
+  imports: [LoginForm],
   templateUrl: './login-card.html',
   styleUrl: './login-card.css',
 })
-export class LoginCard {
-
-}
+export class LoginCard {}
