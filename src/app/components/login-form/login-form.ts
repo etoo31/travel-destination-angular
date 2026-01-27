10653b296a40b4fd02cfc543ece07f5a42f3e92c
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-27 20:36:34 +0200

    feat: added login form html and css and also handling form data and handling on submit

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
 export class LoginForm {
+  errorMessage = signal('');
 
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    userType: new FormControl(''),
+  });
+
+  handleLogin() {
+    const { username, password, userType } = this.loginForm.value;
+    if (username && password && userType) {
+      this.errorMessage.set(
+        `Logging in with\nUsername: ${username}\nPassword: ${password}\nUser Type: ${userType}`,
+      );
+    } else {
+      this.errorMessage.set('Please fill in all fields.');
+    }
+  }
 }
