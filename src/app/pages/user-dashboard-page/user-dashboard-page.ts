cbfa3fcd88d1d330df9bf48c37493ce880681e2e
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-27 22:15:04 +0200

    feat: add user dash board page

---
+++ src/app/pages/user-dashboard-page/user-dashboard-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-dashboard-page',
+  imports: [],
+  templateUrl: './user-dashboard-page.html',
+  styleUrl: './user-dashboard-page.css',
+})
+export class UserDashboardPage {
+
+}
