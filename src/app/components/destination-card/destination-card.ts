80c92c862c2ddb8106eb75f518eeb7c15764ff84
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-28 12:37:11 +0200

    feat: add destination-card component html and css and some logic

---
+++ src/app/components/destination-card/destination-card.ts
@@ -0,0 +1,14 @@
+import { Component, computed, inject } from '@angular/core';
+import { DestinationService } from '../../services/destination-service';
+
+@Component({
+  selector: 'app-destination-card',
+  imports: [],
+  templateUrl: './destination-card.html',
+  styleUrl: './destination-card.css',
+})
+export class DestinationCard {
+  destinationService = inject(DestinationService);
+
+  destinations = computed(() => this.destinationService.getDestinations());
+}
