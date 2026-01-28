63d41c2bf3c5e48989fdd2cca692ab67cc74c94e
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-28 12:40:37 +0200

    update: user-dashboard-page component logic

--- src/app/pages/user-dashboard-page/user-dashboard-page.ts
+++ src/app/pages/user-dashboard-page/user-dashboard-page.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Nav } from '../../components/nav/nav';
+import { DestinationCard } from '../../components/destination-card/destination-card';
+import { NoResult } from '../../components/no-result/no-result';
+import { DestinationService } from '../../services/destination-service';
 
 @Component({
   selector: 'app-user-dashboard-page',
-  imports: [Nav],
+  imports: [Nav, DestinationCard, NoResult],
   templateUrl: './user-dashboard-page.html',
   styleUrl: './user-dashboard-page.css',
 })
-export class UserDashboardPage {}
+export class UserDashboardPage {
+  destinationService = inject(DestinationService);
+
+  destinations = computed(() => this.destinationService.getDestinations());
+}
